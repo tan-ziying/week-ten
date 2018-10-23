9aaa56e1bb449dcaea9581530f9ef9c17b43dae2
jinglejuggle <jingjingzheng_96@hotmail.com>
2018-10-23 15:42:11 +0800

    Contains a failing change function

--- main.spec.js
+++ main.spec.js
@@ -13,6 +13,12 @@ describe("Vue component tests", () => {
   it("The component should have an updateCurrentChart function.", () => {
     expect(typeof App.methods.updateCurrentChart).toBe("function");
   });
+
+  //new change function
+  it("The component should have an change function.", () => {
+    expect(typeof App.methods.change).toBe("function");
+  });
+
   it("has a created hook", () => {
     expect(typeof App).toBe("object");
   });
